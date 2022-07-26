27183a7562435c1a100cf363a6e64c7d4e08fd83
lenrich <lenrich1403@gmail.com>
2022-07-26 22:29:46 +0800

    Navbar & Links

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+export default function about() {
+    return (
+        <Layout>
+            <div>
+                <h1>About Page</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed commodi, tenetur sunt ipsa nihil a! Dolorem animi assumenda accusantium hic sapiente mollitia fugit tenetur error dolores! Sed cupiditate voluptas eos.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed commodi, tenetur sunt ipsa nihil a! Dolorem animi assumenda accusantium hic sapiente mollitia fugit tenetur error dolores! Sed cupiditate voluptas eos.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed commodi, tenetur sunt ipsa nihil a! Dolorem animi assumenda accusantium hic sapiente mollitia fugit tenetur error dolores! Sed cupiditate voluptas eos.</p>
+            </div>
+        </Layout>
+    )
+}
