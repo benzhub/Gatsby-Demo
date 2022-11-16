cc23f73ce1b2759665ed8c09ec722770d41f4038
benzhub <lenrich1403@gmail.com>
2022-11-16 11:21:51 +0800

    Gatsby routes & Links Navbar

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const about = () => {
+  return (
+    <div>
+        <h1>About Page</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut aliquid reprehenderit a est ex repellat nulla quo nihil eaque facere, totam quasi suscipit perspiciatis harum debitis! Esse minima rerum commodi!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut aliquid reprehenderit a est ex repellat nulla quo nihil eaque facere, totam quasi suscipit perspiciatis harum debitis! Esse minima rerum commodi!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut aliquid reprehenderit a est ex repellat nulla quo nihil eaque facere, totam quasi suscipit perspiciatis harum debitis! Esse minima rerum commodi!</p>
+    </div>
+  )
+}
+
+export default about
---
+++ src/pages/projects/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const index = () => {
+  return (
+    <div>
+        <h2>Portfolio</h2>
+        <h3>Projects & Websites I've Created</h3>
+    </div>
+  )
+}
+
+export default index
