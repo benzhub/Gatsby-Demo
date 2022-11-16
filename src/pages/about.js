b1d8fd8356b666606b7bc9ce5b65a4a8f5218310
benzhub <lenrich1403@gmail.com>
2022-11-16 11:59:34 +0800

    styling components

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,14 +1,28 @@
-import React from 'react'
-
+import React from "react";
+import Layout from "../components/Layout";
 const about = () => {
   return (
-    <div>
+    <Layout>
+      <div>
         <h1>About Page</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut aliquid reprehenderit a est ex repellat nulla quo nihil eaque facere, totam quasi suscipit perspiciatis harum debitis! Esse minima rerum commodi!</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut aliquid reprehenderit a est ex repellat nulla quo nihil eaque facere, totam quasi suscipit perspiciatis harum debitis! Esse minima rerum commodi!</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut aliquid reprehenderit a est ex repellat nulla quo nihil eaque facere, totam quasi suscipit perspiciatis harum debitis! Esse minima rerum commodi!</p>
-    </div>
-  )
-}
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut aliquid
+          reprehenderit a est ex repellat nulla quo nihil eaque facere, totam
+          quasi suscipit perspiciatis harum debitis! Esse minima rerum commodi!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut aliquid
+          reprehenderit a est ex repellat nulla quo nihil eaque facere, totam
+          quasi suscipit perspiciatis harum debitis! Esse minima rerum commodi!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut aliquid
+          reprehenderit a est ex repellat nulla quo nihil eaque facere, totam
+          quasi suscipit perspiciatis harum debitis! Esse minima rerum commodi!
+        </p>
+      </div>
+    </Layout>
+  );
+};
 
-export default about
+export default about;
