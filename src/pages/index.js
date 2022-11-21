ab75deae9e3dd12026816f5f3e2f3d135111753a
benzhub <lenrich1403@gmail.com>
2022-11-21 14:53:01 +0800

    useStaticQuery

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,9 @@
 import React from "react"
 import Layout from "../components/Layout"
 import * as styles from '../styles/home.module.css'
-import { Link, graphql } from "gatsby"
+import { Link } from "gatsby"
 
-export default function Home({ data }) {
-  console.log(data)
-  const { title, description } = data.site.siteMetadata
+export default function Home() {
   return (
     <Layout>  
       <section className={styles.header}>
@@ -16,19 +14,7 @@ export default function Home({ data }) {
           <Link className={styles.btn} to="/projects">My Portfolio Projects</Link>
         </div>
         <img src="./banner.png" alt="site banner" style={{ maxWidth: '100%' }}/>
-        <p>{title} - {description}</p>
       </section>
     </Layout>
   )
 }
-
-export const query = graphql`
-  query SiteInfo {
-    site{
-      siteMetadata {
-        title
-        description
-      }
-    }
-  }
-`
