117d5128ba045149209d37e7871e61ab1f1081a2
benzhub <lenrich1403@gmail.com>
2022-11-21 16:38:33 +0800

    Fetching & Listening Data

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,16 +1,46 @@
 import React from "react";
 import Layout from "../../components/Layout";
 import * as styles from '../../styles/projects.module.css'
+import { graphql, Link } from 'gatsby';
 
-const index = () => {
+const index = ({data}) => {
+  // console.log(data)
+  const projects = data.allMarkdownRemark.nodes
   return (
     <Layout>
       <div className={styles.portfolio}>
         <h2>Portfolio</h2>
         <h3>Projects & Websites I've Created</h3>
+        <div className={styles.projects}>
+          {projects.map(project => (
+            <Link to={`/projects/${project.frontmatter.slug}`} key={project.id}>
+              <div>
+                <h3>{project.frontmatter.title}</h3>
+                <p>{project.frontmatter.stack}</p>
+              </div>
+            </Link>
+          ))}
+        </div>
       </div>
     </Layout>
   );
 };
 
 export default index;
+
+// export page query
+export const query = graphql`
+  query ProjectsPage {
+    allMarkdownRemark {
+      nodes {
+        frontmatter {
+          title
+          stack
+          date
+          slug
+        }
+        id
+      }
+    }
+  }
+`
